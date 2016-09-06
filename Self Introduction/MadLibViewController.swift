d579aa3d641f8d3b60767828003048bd3d505702
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-06 15:35:04 -0400

    Added ViewControllers

---
+++ Self Introduction/MadLibViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MadLibViewController.swift
+//  Self Introduction
+//
+//  Created by Dan Esrey on 2016/06/09.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+import UIKit
+
+class MadLibController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
