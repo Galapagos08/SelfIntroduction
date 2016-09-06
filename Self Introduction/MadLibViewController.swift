cfa0b7ba7ee253f7fd000bd1c6ec05d91825f10b
Dan Esrey <danesrey@dans-macbook-air.local>
2016-09-06 19:14:32 -0400

    example

--- Self Introduction/MadLibViewController.swift
+++ Self Introduction/MadLibViewController.swift
@@ -10,10 +10,50 @@ import UIKit
 
 class MadLibController: UIViewController {
     
+    var userEntries = [String]()
+    var adjective: String?
+    var noun: String?
+    var shouldAllowMadlib: Bool {
+        return userEntries.count >= 2
+    }
+    
+    var madlibButtonIsHidden: Bool {
+        return !shouldAllowMadlib
+    }
+    
+    @IBOutlet var textField: UITextField!
+    @IBAction func enter(_ sender: AnyObject) {
+        if (userEntries.count == 0) {
+            if let input = textField.text, input.characters.count > 0 {
+                adjective = input
+                userEntries.append(adjective!)
+            } else if (userEntries.count == 1) {
+                if let input = textField.text, input.characters.count > 0 {
+                    noun = input
+                    userEntries.append(noun!)
+                } else {
+                    print("Text field is empty")
+                }
+            }
+        }
+        madlibButton.isHidden = madlibButtonIsHidden
+    }
+    
+    @IBOutlet var madlibButton: UIButton!
+    
+    @IBAction func goToMadLib(_ sender: AnyObject) {
+        
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+/*    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! MadLib2ViewController
+        destination.name = name
+    } */
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
